00a01abb9ddf7e2f5a03925a9da713751cc8cf97
Philipp Lenssen <philipp.lenssen@gmail.com>
2019-10-08 02:51:16 +0200

    Fixes indent

--- Enums/AttachmentPointId.cs
+++ Enums/AttachmentPointId.cs
@@ -3,15 +3,15 @@
 [Serializable]
 public enum AttachmentPointId
 {
-    None            = 10,
-	Head 			= 0,
-	HeadTop 		= 1,
-	TorsoLower 		= 2,
-	TorsoUpper 		= 3,
-	HandLeft 		= 4,
-	HandRight 		= 5,
-	ArmLeft 		= 6,
-	ArmRight 		= 7,
-	LegLeft  		= 8,
-	LegRight  		= 9
+    None        = 10,
+    Head 	= 0,
+    HeadTop 	= 1,
+    TorsoLower 	= 2,
+    TorsoUpper 	= 3,
+    HandLeft 	= 4,
+    HandRight 	= 5,
+    ArmLeft 	= 6,
+    ArmRight 	= 7,
+    LegLeft  	= 8,
+    LegRight  	= 9
 }
